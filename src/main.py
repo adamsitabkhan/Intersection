cb188f1f6d9a0aacf503a8dc209cd29d42cde050
Adam Sitabkhan <adam.sitabkhan@gmail.com>
2024-12-06 01:23:50 -0600

    Added command line argument for choosing polygon side count

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
@@ -5,14 +7,30 @@ import matplotlib.patches as patches
 
 from utils import generate_convex_polygon, transform_polygon
 
-points = generate_convex_polygon(n=6)
-points = transform_polygon(points)
+def main(args):
+    points = generate_convex_polygon(args.sides)
+    points = transform_polygon(points)
 
-fig, ax = plt.subplots()
-polygon = patches.Polygon(points, closed=True, edgecolor='black', facecolor='cyan')
-ax.add_patch(polygon)
-ax.set_aspect('equal', 'box')
-ax.set_xlim(0, 1)
-ax.set_ylim(0, 1)
+    fig, ax = plt.subplots()
+    polygon = patches.Polygon(points, closed=True, edgecolor='black', facecolor='cyan')
+    ax.add_patch(polygon)
+    ax.set_aspect('equal', 'box')
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
 
-plt.show()
+    plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Provide paramaters for intersection generation")
+
+    parser.add_argument(
+        "--sides",
+        type=int,
+        help="An integer input for the number of polygon sides"
+    )
+
+    args = parser.parse_args()
+    
+    main(args)
+
